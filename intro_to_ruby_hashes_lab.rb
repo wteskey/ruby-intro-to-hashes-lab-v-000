84a39f8a462b8950f17d1a3a1646871f3d5b579c
William J Teskey <willjteskey@gmail.com>
2018-04-17 21:42:02 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4, 
-	    :names => {}, :rent_in_dollars => {} }
+	    :names => {}, 
+	    :rent_in_dollars => {} }
 end
 
 def monopoly_with_third_tier
